8113894989f0a85f0526e3465e02f2382fa34485
kol3x <kolaegipet@gmail.com>
2023-09-08 14:42:19 +0300

    created Gameboard and Ship classes

---
+++ src/logic.js
@@ -0,0 +1,127 @@
+class Ship {
+  constructor(len) {
+    this.len = len;
+    this.cords = [];
+    this.timesHit = 0;
+  }
+  hit() {
+    this.timesHit += 1;
+  }
+  isSunk() {
+    if (this.len === this.timesHit) return true;
+    return false;
+  }
+}
+
+class Gameboard {
+  constructor() {
+    // null for empty, 'ship'] for ship, 'hit' for ship hit, 'miss' for miss
+    this.map = Array.from({ length: 10 }, () => Array(10).fill(null));
+    this.ships = [];
+  }
+  placeShip(cords, len, direction) {
+    let cord = null;
+    if (direction === "horizon") {
+      cord = cords[1];
+    } else {
+      cord = cords[0];
+    }
+    if (10 - len - cord < 0) return false;
+
+    let y = cords[0];
+    let x = cords[1];
+
+    let ship = new Ship(len);
+
+    for (let i = 0; i < len; i++) {
+      if (this.map[y][x] === "ship") return false;
+
+      if (adjacentShips(y, x, direction, i, this.map)) return false;
+
+      ship.cords.push([y, x]);
+      this.map[y][x] = "ship";
+      if (direction === "horizon") {
+        x += 1;
+      } else {
+        y += 1;
+      }
+    }
+
+    this.ships.push(ship);
+  }
+
+  allShipsSunk() {
+    for (let i = 0; i < this.ships.length; i++) {
+      for (let j = 0; j < this.ships[i].cords.length; j++) {
+        const [y, x] = this.ships[i].cords[j];  
+        if (this.map[y][x] === 'ship') return false;
+      }
+    }
+    return true;
+  }
+
+  receiveAttack(cords) {
+    let y = cords[0];
+    let x = cords[1];
+
+    switch (this.map[y][x]) {
+      case null:
+        this.map[y][x] = "miss";
+        return true;
+      case "ship":
+        this.map[y][x] = "hit";
+        for (let i = 0; i < this.ships.length; i++) {
+          if (
+            this.ships[i].cords.find(
+              (el) => el[0] === cords[0] && el[1] === cords[1]
+            ) !== undefined
+          )
+            this.ships[i].hit();
+          return true;
+        }
+      case "hit":
+      case "miss":
+        return false;
+    }
+  }
+}
+
+function isCellValid(cell) {
+  return cell !== null && cell !== undefined;
+}
+
+function adjacentShips(y, x, direction, iterable, map) {
+  const toCheck = [-1, 1];
+
+  if (iterable === 0) {
+    for (let i = 0; i < 2; i++) {
+      if (
+        isCellValid(map[y + toCheck[i]]?.[x]) ||
+        isCellValid(map[y]?.[x + toCheck[i]]) ||
+        isCellValid(map[y + toCheck[i]]?.[x + toCheck[i]]) ||
+        isCellValid(map[y - toCheck[i]]?.[x + toCheck[i]])
+      ) {
+        return true;
+      }
+    }
+  } else if (direction === "horizon" && iterable > 0) {
+    if (
+      isCellValid(map[y - 1]?.[x + 1]) ||
+      isCellValid(map[y]?.[x + 1]) ||
+      isCellValid(map[y + 1]?.[x + 1])
+    ) {
+      return true;
+    }
+  } else if (direction === "vertical" && iterable > 0) {
+    if (
+      isCellValid(map[y + 1]?.[x - 1]) ||
+      isCellValid(map[y + 1]?.[x]) ||
+      isCellValid(map[y + 1]?.[x + 1])
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export { Ship, Gameboard };
---
+++ src/tests.test.js
@@ -0,0 +1,174 @@
+import { Ship, Gameboard } from "./logic";
+
+test("ship correct length", () => {
+  let ship = new Ship(3);
+  expect(ship.len).toBe(3);
+});
+
+test("multiple hits", () => {
+  let ship = new Ship(3);
+  ship.hit();
+  ship.hit();
+  expect(ship.timesHit).toBe(2);
+});
+
+test("3-length ship sunking when has 3 hits", () => {
+  let ship = new Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("single hit registers", () => {
+  let ship = new Ship(3);
+  ship.hit();
+  expect(ship.timesHit).toBe(1);
+});
+
+test("add horizontal ship, check last cell", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "horizon");
+  expect(gameboard.map[0][3]).toBe("ship");
+});
+
+test("add horizontal ship, check second to last cell", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "horizon");
+  expect(gameboard.map[0][2]).toBe("ship");
+});
+
+test("add horizontal ship, check first cell", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "horizon");
+  expect(gameboard.map[0][0]).toBe("ship");
+});
+
+test("add vertical ship, check first cell", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "vertical");
+  expect(gameboard.map[0][0]).toBe("ship");
+});
+
+test("add vertical ship, check second cell", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "vertical");
+  expect(gameboard.map[1][0]).toBe("ship");
+});
+
+test("check an empty cell on a board", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "vertical");
+  expect(gameboard.map[5][5]).toBe(null);
+});
+
+test("ship added to ships on a board", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "vertical");
+  expect(gameboard.ships[0]).toBeInstanceOf(Ship);
+});
+
+test("fail to place 2 ships that share cells", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "horizon");
+  expect(gameboard.placeShip([0, 2], 4, "vertical")).toBe(false);
+});
+
+test("fail to place ship beyond edge", () => {
+  let gameboard = new Gameboard();
+  expect(gameboard.placeShip([9, 2], 4, "vertical")).toBe(false);
+});
+
+test("receive attack on empty cell results in a miss", () => {
+  let gameboard = new Gameboard();
+  gameboard.receiveAttack([0, 0]);
+  expect(gameboard.map[0][0]).toBe("miss");
+});
+
+test("receive attack on ship cell results in a hit", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "vertical");
+  gameboard.receiveAttack([0, 0]);
+  expect(gameboard.map[0][0]).toBe("hit");
+});
+
+test("receive attack on ship cell results in a hit inside ship object", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "vertical");
+  gameboard.receiveAttack([0, 0]);
+  expect(gameboard.ships[0].timesHit).toBe(1);
+});
+
+test("hitting same ship cell twice fails", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "vertical");
+  gameboard.receiveAttack([0, 0]);
+  expect(gameboard.receiveAttack([0, 0])).toBe(false);
+});
+
+test("hitting same empty cell twice fails", () => {
+  let gameboard = new Gameboard();
+  gameboard.receiveAttack([0, 0]);
+  console.log(gameboard.map[0][0]);
+  expect(gameboard.receiveAttack([0, 0])).toBe(false);
+});
+
+test("2 ships can't be placed directly next to each other", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([0, 0], 4, "horizon");
+  expect(gameboard.placeShip([1, 0], 4, "horizon")).toBe(false);
+});
+
+test("2 ships can't be placed directly next to each other #2", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([1, 1], 4, "vertical");
+  expect(gameboard.placeShip([3, 2], 4, "horizon")).toBe(false);
+});
+
+
+test("gameboard aware when it's only ship have sunk", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([1, 1], 4, "vertical");
+  gameboard.receiveAttack([1, 1]);
+  gameboard.receiveAttack([2, 1]);
+  gameboard.receiveAttack([3, 1]);
+  gameboard.receiveAttack([4, 1]);
+  expect(gameboard.allShipsSunk()).toBe(true);
+})
+
+test("placing ship giver correct cords of every ship cell to a ship object", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([1, 1], 4, "vertical");
+  expect(gameboard.ships[0].cords[0][0]).toBe(1);
+})
+
+test("gameboard aware when all of it ships have sunk", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([1, 1], 4, "vertical");
+  gameboard.placeShip([3, 3], 3, "horizon");
+  gameboard.receiveAttack([1, 1]);
+  gameboard.receiveAttack([2, 1]);
+  gameboard.receiveAttack([3, 1]);
+  gameboard.receiveAttack([4, 1]);
+
+  gameboard.receiveAttack([3, 3]);
+  gameboard.receiveAttack([3, 4]);
+  gameboard.receiveAttack([3, 5]);
+  
+  expect(gameboard.allShipsSunk()).toBe(true);
+})
+
+test("gameboard aware when not all of it ships have sunk", () => {
+  let gameboard = new Gameboard();
+  gameboard.placeShip([1, 1], 4, "vertical");
+  gameboard.placeShip([3, 3], 3, "horizon");
+  gameboard.receiveAttack([1, 1]);
+  gameboard.receiveAttack([2, 1]);
+  gameboard.receiveAttack([4, 1]);
+
+  gameboard.receiveAttack([3, 3]);
+  gameboard.receiveAttack([4, 4]);
+  gameboard.receiveAttack([4, 5]);
+  
+  expect(gameboard.allShipsSunk()).toBe(false);
+})
